eabc3d4df7f00488fefaeb3559fee6e5121c25bf
maslmy1 <77600610+maslmy1@users.noreply.github.com>
2024-10-31 14:14:56 -0500

    Update vm_setup.sh

--- deploy/vm_setup.sh
+++ deploy/vm_setup.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+# Update and install required packages
+sudo apt update -y
+sudo apt install -y python3-pip
+
+# Install required Python packages
+pip3 install -r requirements.txt
+
+# Set up environment variables
+echo "export AMADEUS_API_KEY='your_api_key'" >> ~/.bashrc
+echo "export AMADEUS_API_SECRET='your_api_secret'" >> ~/.bashrc
+source ~/.bashrc
 
